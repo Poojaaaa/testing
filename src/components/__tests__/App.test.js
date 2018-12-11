bed21246beef3d1e3114b8c567d4fdfad49dcebd
Poojaaaa <Pooja.gajera11@gmail.com>
2018-12-11 15:15:27 -0800

    code reuse with BeforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,12 +4,16 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />); // shallow just render component but not children of it.
+});
+
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />); // shallow just render component but not children of it.
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />); // shallow just render component but not children of it.
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
