094451c4984187a861af9de6cf4d958a50cc444a
Poojaaaa <Pooja.gajera11@gmail.com>
2018-12-11 12:14:09 -0800

    writing test case

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,3 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
+
+it("shows a comment box", () => {
+  const div = document.create("div");
+  ReactDOM.render(<App />, div);
+
+  // Looks inside the div
+  // and checks to see if comment box is in there
+
+  ReactDOM.unmountComponentAtNode(div); // it cleans up after our tests run
+});
