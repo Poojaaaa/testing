fa764c34a22516f77d200973728647ac620d0eeb
Poojaaaa <Pooja.gajera11@gmail.com>
2018-12-11 18:51:32 -0800

    form submit test case

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
+import { wrap } from "module";
 
 let wrapped;
 
@@ -23,4 +24,15 @@ it("has a text area that user can type in", () => {
   });
   wrapped.update();
   expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+// when we simulate we use normal HTML name event
+it("when form is submitted, text area gets emptied", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" }
+  });
+  wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
 });
