e6790d5635072fe3f3ac4a24caacdf367611ee0a
Poojaaaa <Pooja.gajera11@gmail.com>
2018-12-12 13:19:26 -0800

    handling unknown types

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -10,3 +10,8 @@ it("handle actions of type SAVE_COMMENT", () => {
   const newState = commentsReducer([], action);
   expect(newState).toEqual(["New Comment"]);
 });
+
+it("handles action with unknown type", () => {
+  const newState = commentsReducer([], { type: "fifjoisduis" });
+  expect(newState).toEqual([]);
+});
